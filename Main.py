af6f5d68dfb08511eb6f51ac3f6226b9ebab9be7
oladeindepaul <101461115+oladeindepaul@users.noreply.github.com>
2022-05-27 11:08:46 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,35 +1,18 @@
-# Check if a word is an anagrams 
-# Example:
-# find_anagrams("hello") --> False
-# find_anagrams("racecar") --> True
+
+Check if a word is an anagrams 
+Example:
+ find_anagrams("hello") --> False
+ find_anagrams("racecar") --> True
 
 
-def find_anagrams(sorted_word1, sorted_word2):
-    if sorted_word1 == sorted_word2:
-       return True
-    else:
-        return False
+def find_anagrams(word):
+     [assignment] Add your code here
+     
+    a=("nile")
+    b=("line")
+        Print(sorted(a) == sorted(b))
 
-
-
-# Ask for the words to be checked if they are anagrams from users
-star1 = input("Enter the first word to find if it's anagram: ")
-star2 = input("Enter the second word to find if it's anagram: ")
-
-# check the two words given by users are of the same length
-if len(word1) == len(word2):
-    # change words to lower cases
-    lower_word1 = word1.lower()
-    lower_word2 = word2.lower()
-
-    # sort the words alphabetically 
-    sorted_word1 = sorted(lower_word1)
-    sorted_word2 = sorted(lower_word2)
-else:
-    pass
-    
-try:
-    # call the function
-    print(find_anagrams(sorted_word1, sorted_word2))  
-except:
-    print("so sorry,the words you have entered are not of the same length, so the function can not be called,you can try again")
+     a=("spot")
+     b=("opts")
+     c=("tops")
+         print(sorted(a) == sorted(b) == sorted(c))
